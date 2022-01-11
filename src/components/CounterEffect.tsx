d9b735fa478f67236f03e3908c51cf0efd7da2e7
Adrian Antanon Orozco <adrianantanon@gmail.com>
2022-01-11 14:43:25 +0100

    feat: add useRef

--- src/components/CounterEffect.tsx
+++ src/components/CounterEffect.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { gsap } from "gsap";
+import { TLSSocket } from "tls";
 
 const MAXIMUN_COUNT = 10;
 
 export const CounterEffect: React.FC = () => {
   const [counter, setCounter] = useState(5);
+  const counterElement = useRef<HTMLHeadingElement>(null);
 
   const handleClick = (amount: number = 1): void => {
     setCounter((prevCounter) =>
@@ -18,11 +21,25 @@ export const CounterEffect: React.FC = () => {
       "%cCounter llegó al valor máximo",
       "color: red; background-color: black"
     );
+    const timeline = gsap.timeline();
+
+    timeline
+      .to(counterElement.current, {
+        y: -10,
+        duration: 0.2,
+        ease: "ease.out",
+      })
+      .to(counterElement.current, {
+        y: 0,
+        duration: 1,
+        ease: "bounce.out",
+      });
   }, [counter]);
 
   return (
     <>
-      <h1>CounterEffect: {counter}</h1>
+      <h1>CounterEffect</h1>
+      <h2 ref={counterElement}>{counter}</h2>
       <button onClick={() => handleClick()}>+1</button>
     </>
   );
